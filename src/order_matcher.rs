8f33124fbcaa04e06fe95fa0d06649fe7ab6fa9c
Max.0x90 <maximilian@in-one-audio.com>
2021-08-10 20:37:45 +0200

    Cargo fmt

--- src/order_matcher.rs
+++ src/order_matcher.rs
@@ -1,17 +1,17 @@
-use crate::orderbook::{Orderbook, Order};
+use crate::orderbook::{Order, Orderbook};
+use crate::symbol::AskOrBid;
 use crate::OrderId;
 use rust_decimal::Decimal;
-use crate::symbol::AskOrBid;
 
 pub struct Maker {
     order_id: OrderId,
     price: Decimal,
-    filled: Decimal
+    filled: Decimal,
 }
 
 pub struct Match {
     pub taker: OrderId,
-    pub makers: Vec<(OrderId, Decimal)> //OrderId, filled
+    pub makers: Vec<(OrderId, Decimal)>, //OrderId, filled
 }
 
 impl Match {
@@ -19,16 +19,29 @@ impl Match {
     pub fn new(taker: OrderId) -> Match {
         Match {
             taker,
-            makers: Vec::new()
+            makers: Vec::new(),
         }
     }
 }
 
 pub enum MatchError {
-    CantTrade
+    CantTrade,
 }
 
 pub trait OrderMatcher {
-    fn match_limit(&self, orderbook: &mut Orderbook, order_id: &OrderId, side: AskOrBid, price: &Decimal, amount: &Decimal,) -> Option<Match>;
-    fn match_market(&self, orderbook: &mut Orderbook, order: &Order) -> Option<Match>;
+    fn match_limit(
+        &self,
+        orderbook: &mut Orderbook,
+        order_id: &OrderId,
+        side: AskOrBid,
+        price: &Decimal,
+        amount: &Decimal,
+    ) -> Option<Match>;
+    fn match_market(
+        &self,
+        orderbook: &mut Orderbook,
+        order_id: &OrderId,
+        side: AskOrBid,
+        amount: &Decimal,
+    ) -> Option<Match>;
 }
--- src/order_matcher_fifo.rs
+++ src/order_matcher_fifo.rs
@@ -1,42 +1,50 @@
-use crate::order_matcher::{OrderMatcher, Match, MatchError};
-use crate::orderbook::{Orderbook, Order, OrderbookPage};
-use crate::OrderId;
+use crate::order_matcher::{Match, MatchError, OrderMatcher};
+use crate::orderbook::{Order, Orderbook, OrderbookPage};
 use crate::symbol::AskOrBid;
-use rust_decimal::Decimal;
-use rust_decimal::prelude::Zero;
+use crate::OrderId;
 use log::info;
 use log::Level::Debug;
+use rust_decimal::prelude::Zero;
+use rust_decimal::Decimal;
 
-pub struct OrderMatcherFifo {
-
-}
+pub struct OrderMatcherFifo {}
 
 impl OrderMatcherFifo {
     pub fn new() -> OrderMatcherFifo {
-        OrderMatcherFifo{}
+        OrderMatcherFifo {}
     }
 }
 
 impl OrderMatcher for OrderMatcherFifo {
-    //TODO: Limit fill best price not the price specified
-    fn match_limit(&self, orderbook: &mut Orderbook, order_id: &OrderId, side: AskOrBid, price: &Decimal, amount: &Decimal) -> Option<Match> {
+    fn match_limit(
+        &self,
+        orderbook: &mut Orderbook,
+        order_id: &OrderId,
+        side: AskOrBid,
+        price: &Decimal,
+        amount: &Decimal,
+    ) -> Option<Match> {
         let (orderbook_maker, orderbook_taker) = match side {
             AskOrBid::Ask => (&mut orderbook.orders_bid, &mut orderbook.orders_ask),
-            AskOrBid::Bid => (&mut orderbook.orders_ask, &mut orderbook.orders_bid)
+            AskOrBid::Bid => (&mut orderbook.orders_ask, &mut orderbook.orders_bid),
         };
 
-        let mut order = Order{ id: order_id.clone(), unfilled: amount.clone() };
+        let mut order = Order {
+            id: order_id.clone(),
+            unfilled: amount.clone(),
+        };
         let mut makers: Vec<(OrderId, Decimal)> = Vec::new(); //Decimal = filled
-        let mut page_to_remove =  None;
+        let mut page_to_remove = None;
 
         'page_loop: loop {
             let mut tuple = match side {
-                AskOrBid::Bid => orderbook_maker.iter_mut().find(|(page_price, _)| {
-                    *page_price <= price
-                }),
-                AskOrBid::Ask => orderbook_maker.iter_mut().rev().find(|(page_price, _)| {
-                    *page_price >= price
-                })
+                AskOrBid::Bid => orderbook_maker
+                    .iter_mut()
+                    .find(|(page_price, _)| *page_price <= price),
+                AskOrBid::Ask => orderbook_maker
+                    .iter_mut()
+                    .rev()
+                    .find(|(page_price, _)| *page_price >= price),
             };
 
             /*let mut can_trade;
@@ -71,11 +79,12 @@ impl OrderMatcher for OrderMatcherFifo {
                 //break;
             }*/
 
-            if let Some((page_price, ref mut  page)) = tuple {
+            if let Some((page_price, ref mut page)) = tuple {
                 'order_loop: loop {
                     if let Some(mut maker_entry) = page.orders.entries().next() {
                         let mut maker_order = maker_entry.get_mut();
-                        if maker_order.unfilled > order.unfilled { //Maker can filly absorb the (remaining) order
+                        if maker_order.unfilled > order.unfilled {
+                            //Maker can filly absorb the (remaining) order
                             maker_order.unfilled -= order.unfilled;
                             page.amount -= order.unfilled;
                             makers.push((maker_order.id, order.unfilled));
@@ -91,20 +100,23 @@ impl OrderMatcher for OrderMatcherFifo {
                             maker_entry.remove();
                             page_to_remove = Some(page_price.clone());
                         }
-                    } else { //No more orders left on page
-                        break 'order_loop
+                    } else {
+                        //No more orders left on page
+                        break 'order_loop;
                     }
-                    if order.unfilled == Decimal::zero() { //Order fully matched
-                        break 'page_loop
+                    if order.unfilled == Decimal::zero() {
+                        //Order fully matched
+                        break 'page_loop;
                     }
                 }
             } else {
-                break 'page_loop //No pages left
+                break 'page_loop; //No pages left
             }
 
             //Delete page in case no orders are left
             if let Some(page_to_remove) = page_to_remove {
-                if let Some(page) = orderbook_maker.get(&page_to_remove) { //Delete page when empty
+                if let Some(page) = orderbook_maker.get(&page_to_remove) {
+                    //Delete page when empty
                     if page.orders.is_empty() {
                         orderbook_maker.remove(&page_to_remove);
                     }
@@ -116,31 +128,38 @@ impl OrderMatcher for OrderMatcherFifo {
             orderbook._insert_limit(order.clone(), side, price.clone());
         }
 
-        match order.unfilled == *amount { //Match whether any orders have been matched at all
+        match order.unfilled == *amount {
+            //Match whether any orders have been matched at all
             true => None,
-            false => {
-                Some(Match {
-                    taker: order.id.clone(),
-                    makers
-                })
-            }
+            false => Some(Match {
+                taker: order.id.clone(),
+                makers,
+            }),
         }
     }
 
-    fn match_market(&self, orderbook: &mut Orderbook, order: &Order) -> Option<Match> {
-        todo!()
-
+    fn match_market(
+        &self,
+        orderbook: &mut Orderbook,
+        order_id: &OrderId,
+        side: AskOrBid,
+        amount: &Decimal,
+    ) -> Option<Match> {
+        match side {
+            AskOrBid::Ask => self.match_limit(orderbook, order_id, side, &Decimal::ZERO, amount),
+            AskOrBid::Bid => self.match_limit(orderbook, order_id, side, &Decimal::MAX, amount),
+        }
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::orderbook::Orderbook;
-    use crate::symbol::{Symbol, AskOrBid};
-    use crate::order_matcher_fifo::OrderMatcherFifo;
     use crate::order_matcher::OrderMatcher;
-    use rust_decimal::Decimal;
+    use crate::order_matcher_fifo::OrderMatcherFifo;
+    use crate::orderbook::Orderbook;
+    use crate::symbol::{AskOrBid, Symbol};
     use rust_decimal::prelude::ToPrimitive;
+    use rust_decimal::Decimal;
 
     #[test]
     fn test_match_limit_single_price_level() {
@@ -155,35 +174,57 @@ mod tests {
         let price = Decimal::from(512);
 
         //Write 2 ask orders at the same price
-        assert_eq!(matcher.match_limit(
-            &mut orderbook,
-            &first_maker_id,
-            AskOrBid::Ask,
-            &price,
-            &first_maker_amount
-        ).is_none(), true);
+        assert_eq!(
+            matcher
+                .match_limit(
+                    &mut orderbook,
+                    &first_maker_id,
+                    AskOrBid::Ask,
+                    &price,
+                    &first_maker_amount
+                )
+                .is_none(),
+            true
+        );
         assert_eq!(orderbook.orders_ask.get(&price).is_some(), true);
-        assert_eq!(orderbook.orders_ask.get(&price).unwrap().amount, first_maker_amount);
-        let (id, order) = orderbook.orders_ask.get(&price).unwrap().orders.iter().next().unwrap();
+        assert_eq!(
+            orderbook.orders_ask.get(&price).unwrap().amount,
+            first_maker_amount
+        );
+        let (id, order) = orderbook
+            .orders_ask
+            .get(&price)
+            .unwrap()
+            .orders
+            .iter()
+            .next()
+            .unwrap();
         assert_eq!(order.unfilled, first_maker_amount);
 
-        assert_eq!(matcher.match_limit(
-            &mut orderbook,
-            &second_maker_id,
-            AskOrBid::Ask,
-            &price,
-                &Decimal::from( 16)
-        ).is_none(), true);
+        assert_eq!(
+            matcher
+                .match_limit(
+                    &mut orderbook,
+                    &second_maker_id,
+                    AskOrBid::Ask,
+                    &price,
+                    &Decimal::from(16)
+                )
+                .is_none(),
+            true
+        );
 
         //Write a limit order that will be matched
         let mut taker_id = 2;
-        let result = matcher.match_limit(
-            &mut orderbook,
-            &taker_id,
-            AskOrBid::Bid,
-            &price,
-            &Decimal::from(31)
-        ).unwrap();
+        let result = matcher
+            .match_limit(
+                &mut orderbook,
+                &taker_id,
+                AskOrBid::Bid,
+                &price,
+                &Decimal::from(31),
+            )
+            .unwrap();
 
         //Check match result
         assert_eq!(result.taker, taker_id);
@@ -194,17 +235,22 @@ mod tests {
         assert_eq!(orderbook.orders_index.get(&first_maker_id).is_some(), true); //Maker order hasn't been fully filled yet
 
         //Remaining amount on the page
-        assert_eq!(orderbook.orders_ask.get(&price).unwrap().amount, Decimal::from(17));
+        assert_eq!(
+            orderbook.orders_ask.get(&price).unwrap().amount,
+            Decimal::from(17)
+        );
 
         //Limit that has two takers
         taker_id += 1;
-        let result = matcher.match_limit(
-            &mut orderbook,
-            &taker_id,
-            AskOrBid::Bid,
-            &price,
-            &Decimal::from(16)
-        ).unwrap();
+        let result = matcher
+            .match_limit(
+                &mut orderbook,
+                &taker_id,
+                AskOrBid::Bid,
+                &price,
+                &Decimal::from(16),
+            )
+            .unwrap();
 
         assert_eq!(result.taker, taker_id);
         assert_eq!(result.makers.len(), 2);
@@ -219,17 +265,22 @@ mod tests {
         assert_eq!(orderbook.orders_index.get(&first_maker_id).is_some(), false);
 
         //Remaining amount on the page
-        assert_eq!(orderbook.orders_ask.get(&price).unwrap().amount, Decimal::from(1));
+        assert_eq!(
+            orderbook.orders_ask.get(&price).unwrap().amount,
+            Decimal::from(1)
+        );
 
         //Take more than remaining amount
         taker_id += 1;
-        let result = matcher.match_limit(
-            &mut orderbook,
-            &taker_id,
-            AskOrBid::Bid,
-            &price,
-            &Decimal::from(11)
-        ).unwrap();
+        let result = matcher
+            .match_limit(
+                &mut orderbook,
+                &taker_id,
+                AskOrBid::Bid,
+                &price,
+                &Decimal::from(11),
+            )
+            .unwrap();
 
         //Page gets removed after last order is filled
         assert_eq!(orderbook.orders_ask.get(&price).is_none(), true);
@@ -263,7 +314,7 @@ mod tests {
             &first_maker_id,
             AskOrBid::Ask,
             &price1,
-            &first_maker_amount
+            &first_maker_amount,
         );
 
         matcher.match_limit(
@@ -271,17 +322,19 @@ mod tests {
             &second_maker_id,
             AskOrBid::Ask,
             &price2,
-            &first_maker_amount
+            &first_maker_amount,
         );
 
         //Place limit at higher ask level, check if the lowest order gets hit
-        let result = matcher.match_limit(
-            &mut orderbook,
-            &3,
-            AskOrBid::Bid,
-            &Decimal::ONE_THOUSAND,
-            &first_maker_amount
-        ).unwrap();
+        let result = matcher
+            .match_limit(
+                &mut orderbook,
+                &3,
+                AskOrBid::Bid,
+                &Decimal::ONE_THOUSAND,
+                &first_maker_amount,
+            )
+            .unwrap();
         assert_eq!(result.makers.len(), 1);
 
         let mut iter = result.makers.iter();
@@ -308,7 +361,7 @@ mod tests {
             &first_maker_id,
             AskOrBid::Ask,
             &price1,
-            &first_maker_amount
+            &first_maker_amount,
         );
 
         matcher.match_limit(
@@ -316,22 +369,27 @@ mod tests {
             &second_maker_id,
             AskOrBid::Ask,
             &price2,
-            &first_maker_amount
+            &first_maker_amount,
         );
 
         let price_limit = &(price2 - Decimal::ONE);
-        let result = matcher.match_limit(
-            &mut orderbook,
-            &3,
-            AskOrBid::Bid,
-            &price_limit,
-            &Decimal::ONE_HUNDRED
-        ).unwrap();
+        let result = matcher
+            .match_limit(
+                &mut orderbook,
+                &3,
+                AskOrBid::Bid,
+                &price_limit,
+                &Decimal::ONE_HUNDRED,
+            )
+            .unwrap();
 
         let page_written = orderbook.orders_bid.get(&price_limit);
         assert_eq!(page_written.is_some(), true);
         let page_written = page_written.unwrap();
-        assert_eq!(page_written.amount, Decimal::ONE_HUNDRED - first_maker_amount);
+        assert_eq!(
+            page_written.amount,
+            Decimal::ONE_HUNDRED - first_maker_amount
+        );
         assert_eq!(page_written.orders.len(), 1);
 
         let order_written = page_written.orders.iter().next();
@@ -356,7 +414,7 @@ mod tests {
             &order_id,
             AskOrBid::Bid,
             &Decimal::from(4233),
-            &first_maker_amount
+            &first_maker_amount,
         );
 
         order_id += 1;
@@ -365,7 +423,7 @@ mod tests {
             &order_id,
             AskOrBid::Bid,
             &Decimal::from(700),
-            &first_maker_amount
+            &first_maker_amount,
         );
 
         order_id += 1;
@@ -374,7 +432,7 @@ mod tests {
             &order_id,
             AskOrBid::Bid,
             &Decimal::from(700),
-            &first_maker_amount
+            &first_maker_amount,
         );
 
         let price_no_touch = Decimal::from(678);
@@ -385,23 +443,37 @@ mod tests {
             &order_id,
             AskOrBid::Bid,
             &price_no_touch,
-            &first_maker_amount
+            &first_maker_amount,
         );
 
         //Execute order
         order_id += 1;
-        let result = matcher.match_limit(
-            &mut orderbook,
-            &order_id,
-            AskOrBid::Ask,
-            &Decimal::from(679),
-            &(Decimal::from(4) * first_maker_amount)
-        ).unwrap();
+        let result = matcher
+            .match_limit(
+                &mut orderbook,
+                &order_id,
+                AskOrBid::Ask,
+                &Decimal::from(679),
+                &(Decimal::from(4) * first_maker_amount),
+            )
+            .unwrap();
 
         //One order remaining on bid
         assert_eq!(orderbook.orders_bid.len(), 1);
         assert_eq!(orderbook.orders_bid.get(&price_no_touch).is_some(), true);
-        assert_eq!(orderbook.orders_bid.get(&price_no_touch).unwrap().orders.iter().next().unwrap().1.id, order_id - 1);
+        assert_eq!(
+            orderbook
+                .orders_bid
+                .get(&price_no_touch)
+                .unwrap()
+                .orders
+                .iter()
+                .next()
+                .unwrap()
+                .1
+                .id,
+            order_id - 1
+        );
 
         //Matched against three makers
         assert_eq!(result.makers.len(), 3);
